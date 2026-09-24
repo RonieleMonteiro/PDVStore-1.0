0cc7327626ef18dbd538c4149b0e8c6b21a8fd3a
SalomaoTarga <salomaotarga2009@gmail.com>
2026-09-24 16:04:55 -0300

    Arrumei o formulario de GerenciarProdutos

--- Forms/frmGerenciarProdutos.cs
+++ Forms/frmGerenciarProdutos.cs
@@ -51,7 +51,9 @@ namespace PDVStore.Forms
         {
             Text = "Gerenciar Produtos";
             StartPosition = FormStartPosition.CenterScreen;
-            ClientSize = new Size(1180, 560);
+            ClientSize = new Size(1200, 560);
+            MaximumSize = new Size(1200, 560);
+            MinimumSize = new Size(1200, 560);
             Font = new Font("Segoe UI", 10F);
             BackColor = Color.White;
 
